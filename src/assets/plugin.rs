ef9b249fa447222e26f3b2f0f2c2bd54a2d522df
Carlo Supina <cdsupina@gmail.com>
2024-12-25 23:20:26 -0600

    embed assets into binary using EmbeddedAssetsPlugin

--- src/assets/plugin.rs
+++ src/assets/plugin.rs
@@ -4,13 +4,17 @@ use bevy::app::Plugin;
 use bevy_asset_loader::loading_state::{
     config::ConfigureLoadingState, LoadingState, LoadingStateAppExt,
 };
+use bevy_embedded_assets::{EmbeddedAssetPlugin, PluginMode};
 
 /// Plugin for managing asset loading states in Thetawave
 pub(crate) struct ThetawaveAssetsPlugin;
 
 impl Plugin for ThetawaveAssetsPlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
-        app.add_loading_state(
+        app.add_plugins(EmbeddedAssetPlugin {
+            mode: PluginMode::ReplaceDefault, //embeds assets into binary
+        })
+        .add_loading_state(
             LoadingState::new(AppState::MainMenuLoading)
                 .continue_to_state(AppState::MainMenu)
                 .load_collection::<MainMenuAssets>()
