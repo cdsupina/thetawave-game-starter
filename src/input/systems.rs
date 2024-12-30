d59cf18ea279bd652a09db04749b4666ce6599d6
Carlo Supina <cdsupina@gmail.com>
2024-12-29 20:26:35 -0600

    disable free key binding

--- src/input/systems.rs
+++ src/input/systems.rs
@@ -1,10 +1,15 @@
-use bevy::prelude::{KeyCode, ResMut};
+use bevy::{
+    input::keyboard::NativeKeyCode,
+    prelude::{KeyCode, ResMut},
+};
 use bevy_alt_ui_navigation_lite::systems::InputMapping;
 
 /// Setup function for input mapping configuration
 pub(super) fn setup_input_system(mut input_mapping: ResMut<InputMapping>) {
     // Set action keyboard binding to enter
     input_mapping.key_action = KeyCode::Enter;
+    // Disable key_free binding
+    input_mapping.key_free = KeyCode::Unidentified(NativeKeyCode::Unidentified);
 
     // Sets focus to follow mouse movement and enables keyboard navigation
     input_mapping.focus_follows_mouse = true;
