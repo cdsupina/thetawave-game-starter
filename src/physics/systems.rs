092423c03c73050795d26b81bc3ae8ca2d978c44
Carlo Supina <cdsupina@gmail.com>
2025-01-07 12:09:17 -0600

    pause and resume avian physics on gamestate change (#22)

--- src/physics/systems.rs
+++ src/physics/systems.rs
@@ -1,1 +1,25 @@
+use crate::states::GameState;
+use avian2d::prelude::{Physics, PhysicsTime};
+use bevy::{
+    prelude::{EventReader, ResMut, StateTransitionEvent},
+    time::Time,
+};
 
+/// Pause and resume physics on GameState change
+pub(super) fn pause_physics_system(
+    mut physics_time: ResMut<Time<Physics>>,
+    mut game_state_trans: EventReader<StateTransitionEvent<GameState>>,
+) {
+    for event in game_state_trans.read() {
+        if let Some(entered_state) = event.entered {
+            match entered_state {
+                GameState::Playing => {
+                    physics_time.unpause();
+                }
+                GameState::Paused => {
+                    physics_time.pause();
+                }
+            }
+        }
+    }
+}
