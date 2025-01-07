1a7a35508daffd4b9fc75c3250fb9cb5247feb1d
cdsupina <cdsupina@gmail.com>
2025-01-07 12:07:59 -0600

    pause and resume avian physics on gamestate change

--- src/physics/plugin.rs
+++ src/physics/plugin.rs
@@ -1,11 +1,19 @@
+use super::systems::pause_physics_system;
+use crate::states::AppState;
 use avian2d::{prelude::PhysicsDebugPlugin, PhysicsPlugins};
-use bevy::app::Plugin;
+use bevy::{
+    app::{Plugin, Update},
+    prelude::{in_state, IntoSystemConfigs},
+};
 
 pub(crate) struct ThetawavePhysicsPlugin;
 
 impl Plugin for ThetawavePhysicsPlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
-        app.add_plugins(PhysicsPlugins::default());
+        app.add_plugins(PhysicsPlugins::default()).add_systems(
+            Update,
+            pause_physics_system.run_if(in_state(AppState::Game)),
+        );
 
         if cfg!(feature = "physics_debug") {
             app.add_plugins(PhysicsDebugPlugin::default());
