f0a41e5e7f8b34e47a2c8ef2b3ab9c401c6ef154
Carlo Supina <cdsupina@gmail.com>
2025-01-27 01:23:08 -0600

    Window icon and title (#31)
    * add window title and window constants
    
    * system for applying window icon

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use bevy::{
-    app::App,
-    prelude::{DefaultPlugins, ImagePlugin, PluginGroup},
+    app::{App, Startup},
+    prelude::{DefaultPlugins, ImagePlugin, NonSend, PluginGroup},
     utils::default,
     window::{Window, WindowMode, WindowPlugin, WindowResolution},
+    winit::WinitWindows,
 };
 use bevy_aseprite_ultra::AsepriteUltraPlugin;
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use winit::window::Icon;
 
 mod assets;
 mod audio;
@@ -18,6 +20,9 @@ mod player;
 mod states;
 mod ui;
 
+const PRIMARY_WINDOW_TITLE: &str = "Thetawave Starter Template";
+const STARTING_WINDOW_RESOLUTION: (f32, f32) = (1280.0, 720.0);
+
 fn main() {
     let mut app = App::new();
 
@@ -28,9 +33,10 @@ fn main() {
             .set(WindowPlugin {
                 primary_window: Some(Window {
                     mode: WindowMode::Windowed,
-                    resolution: WindowResolution::new(1280.0, 720.0)
+                    resolution: WindowResolution::from(STARTING_WINDOW_RESOLUTION)
                         .with_scale_factor_override(1.0),
                     resizable: false,
+                    title: PRIMARY_WINDOW_TITLE.to_string(),
                     ..default()
                 }),
                 ..default()
@@ -46,7 +52,8 @@ fn main() {
         audio::ThetawaveAudioPlugin,
         player::ThetawavePlayerPlugin,
         physics::ThetawavePhysicsPlugin,
-    ));
+    ))
+    .add_systems(Startup, set_window_icon_system);
 
     if cfg!(feature = "world_inspector") {
         println!("here");
@@ -54,4 +61,26 @@ fn main() {
     }
 
     app.run();
+}
+
+fn set_window_icon_system(
+    // we have to use `NonSend` here
+    windows: NonSend<WinitWindows>,
+) {
+    // here we use the `image` crate to load our icon data from a png file
+    // this is not a very bevy-native solution, but it will do
+    let (icon_rgba, icon_width, icon_height) = {
+        let image = image::open("assets/window_icon.png")
+            .expect("Failed to open icon path")
+            .into_rgba8();
+        let (width, height) = image.dimensions();
+        let rgba = image.into_raw();
+        (rgba, width, height)
+    };
+    let icon = Icon::from_rgba(icon_rgba, icon_width, icon_height).unwrap();
+
+    // do it for all windows
+    for window in windows.windows.values() {
+        window.set_window_icon(Some(icon.clone()));
+    }
 }
