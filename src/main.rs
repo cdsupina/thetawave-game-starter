82ed2edac3d8fc8952f7179322e59a4b642e416e
Carlo Supina <cdsupina@gmail.com>
2025-01-27 22:36:48 -0600

    Add Window Module (#32)
    * seperate module for window
    
    * update comments

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 use bevy::{
-    app::{App, Startup},
-    prelude::{DefaultPlugins, ImagePlugin, NonSend, PluginGroup},
+    app::App,
+    prelude::{DefaultPlugins, ImagePlugin, PluginGroup},
     utils::default,
     window::{Window, WindowMode, WindowPlugin, WindowResolution},
-    winit::WinitWindows,
 };
 use bevy_aseprite_ultra::AsepriteUltraPlugin;
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
-use winit::window::Icon;
 
 mod assets;
 mod audio;
@@ -19,6 +17,7 @@ mod physics;
 mod player;
 mod states;
 mod ui;
+mod window;
 
 const PRIMARY_WINDOW_TITLE: &str = "Thetawave Starter Template";
 const STARTING_WINDOW_RESOLUTION: (f32, f32) = (1280.0, 720.0);
@@ -52,8 +51,8 @@ fn main() {
         audio::ThetawaveAudioPlugin,
         player::ThetawavePlayerPlugin,
         physics::ThetawavePhysicsPlugin,
-    ))
-    .add_systems(Startup, set_window_icon_system);
+        window::ThetawaveWindowPlugin,
+    ));
 
     if cfg!(feature = "world_inspector") {
         println!("here");
@@ -61,26 +60,4 @@ fn main() {
     }
 
     app.run();
-}
-
-fn set_window_icon_system(
-    // we have to use `NonSend` here
-    windows: NonSend<WinitWindows>,
-) {
-    // here we use the `image` crate to load our icon data from a png file
-    // this is not a very bevy-native solution, but it will do
-    let (icon_rgba, icon_width, icon_height) = {
-        let image = image::open("assets/window_icon.png")
-            .expect("Failed to open icon path")
-            .into_rgba8();
-        let (width, height) = image.dimensions();
-        let rgba = image.into_raw();
-        (rgba, width, height)
-    };
-    let icon = Icon::from_rgba(icon_rgba, icon_width, icon_height).unwrap();
-
-    // do it for all windows
-    for window in windows.windows.values() {
-        window.set_window_icon(Some(icon.clone()));
-    }
 }
