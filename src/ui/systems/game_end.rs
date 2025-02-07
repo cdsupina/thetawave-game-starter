04a571d41dcdd5d41d90306565923633ccf30000
cdsupina <cdsupina@gmail.com>
2025-02-07 12:42:00 -0600

    fill out game end screen with title text and center node

--- src/ui/systems/game_end.rs
+++ src/ui/systems/game_end.rs
@@ -3,7 +3,11 @@ use bevy::{
     core::Name,
     ecs::system::{Commands, Res},
     hierarchy::{BuildChildren, ChildBuild},
-    ui::{AlignItems, BackgroundColor, Display, FlexDirection, JustifyContent, Node, UiRect, Val},
+    text::TextFont,
+    ui::{
+        widget::Text, AlignItems, BackgroundColor, Display, FlexDirection, JustifyContent, Node,
+        UiRect, Val,
+    },
     utils::default,
 };
 
@@ -34,6 +38,41 @@ pub(in crate::ui) fn spawn_game_end_system(mut cmds: Commands, ui_assets: Res<Ui
         BackgroundColor(Color::srgba(0.0, 0.0, 0.0, 0.8)),
     ))
     .with_children(|parent| {
+        parent
+            .spawn(Node {
+                height: Val::Percent(100.0),
+                width: Val::Percent(100.0),
+                flex_direction: FlexDirection::Column,
+                ..default()
+            })
+            .with_children(|parent| {
+                // Top node containing end result/title text of the end screen
+                // Game over, victory, etc
+                parent
+                    .spawn(Node {
+                        height: Val::Percent(40.0),
+                        width: Val::Percent(100.0),
+                        justify_content: JustifyContent::Center,
+                        align_items: AlignItems::Center,
+                        ..default()
+                    })
+                    .with_children(|parent| {
+                        parent.spawn((
+                            Text::new("GAME OVER"),
+                            TextFont::from_font_size(150.0)
+                                .with_font(ui_assets.dank_depths_font.clone()),
+                        ));
+                    });
+
+                // Center Node of the screen containing for containg information about the run
+                // or high scores, etc
+                parent.spawn(Node {
+                    height: Val::Percent(60.0),
+                    width: Val::Percent(100.0),
+                    ..default()
+                });
+            });
+
         parent
             .spawn(Node {
                 display: Display::Flex,
