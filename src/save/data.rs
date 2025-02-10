9b1f4302c332c98b9e730eed81c95e4da2600687
cdsupina <cdsupina@gmail.com>
2025-02-10 00:03:14 -0600

    increment values in save file

--- src/save/data.rs
+++ src/save/data.rs
@@ -1,9 +1,12 @@
 use bevy::ecs::system::Resource;
 use serde::{Deserialize, Serialize};
 
-#[derive(Default, Resource, Serialize, Deserialize, Clone)]
+/// Save file for persisting player game progress
+#[derive(Default, Resource, Serialize, Deserialize, Clone, Debug)]
 pub(crate) struct SaveRes {
+    /// Number of total games they player has played
     pub run_count: u32,
+    /// Number of times the player has entered the victory or game over ends
     pub win_count: u32,
     pub loss_count: u32,
 }
--- src/save/plugin.rs
+++ src/save/plugin.rs
@@ -1,11 +1,17 @@
-use super::systems::setup_save_res;
-use bevy::app::{Plugin, Startup};
+use super::systems::{increment_run_count_system, increment_win_loss_count_system, setup_save_res};
+use crate::states::{AppState, GameState};
+use bevy::{
+    app::{Plugin, Startup},
+    state::state::OnEnter,
+};
 
 /// Plugin for managing player save files and save data
 pub(crate) struct ThetawaveSavePlugin;
 
 impl Plugin for ThetawaveSavePlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
-        app.add_systems(Startup, setup_save_res);
+        app.add_systems(Startup, setup_save_res)
+            .add_systems(OnEnter(AppState::Game), increment_run_count_system)
+            .add_systems(OnEnter(GameState::End), increment_win_loss_count_system);
     }
 }
